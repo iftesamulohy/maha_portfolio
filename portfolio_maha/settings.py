dfbffd6daa28ffc8cecc61627301d8da2ac8f54b
iftesamulohy <64037194+iftesamulohy@users.noreply.github.com>
2022-10-02 02:28:14 +0600

    updated code

--- portfolio_maha/settings.py
+++ portfolio_maha/settings.py
@@ -122,8 +122,8 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR , "staticfiles")
-STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / "staticfiles"
+STATIC_URL = 'static/'
 STATIC_FILES=[
     BASE_DIR/"static"
 ]
@@ -131,6 +131,6 @@ STATIC_FILES=[
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-MEDIA_ROOT = os.path.join(BASE_DIR,"uploads")
+MEDIA_ROOT = BASE_DIR/"uploads"
 MEDIA_URL = "/user-media/"
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
