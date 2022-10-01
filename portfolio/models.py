2a462f6df14184ca74170681189e82d074db88e7
iftesamulohy <64037194+iftesamulohy@users.noreply.github.com>
2022-10-02 03:31:12 +0600

    updated code

--- portfolio/models.py
+++ portfolio/models.py
@@ -2,7 +2,7 @@ import email
 from email.mime import image
 from locale import currency
 from pyexpat import model
-from tkinter import Widget
+#from tkinter import Widget
 from turtle import title
 from unicodedata import category
 from django.db import models
